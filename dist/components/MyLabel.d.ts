407a5074f5262e76a2089c45b01f7e636275b61a
Miguel <miguelenrique1805@gmail.com>
2022-08-31 09:26:49 -0500

    feat: add semantic-version

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="react" />
+import "./mylabel.css";
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar en la etiqueta
+     */
+    label: string;
+    /**
+     * Este es el tamaño de la etiqueta
+     */
+    size: "normal" | "h1" | "h2" | "h3";
+    /**
+     * Si quiere todo capitalizado
+     */
+    allCaps?: boolean;
+    /**
+     * Colores básicos del botón
+     */
+    color?: "primary" | "secondary" | "tertiary";
+    /**
+     * Color personalizado de la fuente
+     */
+    fontColor?: string;
+    /**
+     * Color personalizado de la fuente
+     */
+    backgroundColor?: string;
+    /**
+     * Color personalizado de la fuente
+     */
+    font?: "monospace" | "sans-serif" | "fantasy";
+}
+export declare const MyLabel: ({ allCaps, color, label, size, fontColor, font, backgroundColor, }: MyLabelProps) => JSX.Element;
